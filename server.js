751e9ed3bf9e6baf71430b1a3b0e8635bfafcc4a
Ermiyas2034 <ermiyas2034@gmail.com>
2024-03-21 05:04:59 +0300

    server config

---
+++ server.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client connected!');
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    // Broadcast incoming message to all clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('WebSocket server started on ws://localhost:8080');
